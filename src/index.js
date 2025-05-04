593e69f4b09a8da2a8c0880b9441c1f83181b43c
Ash Luchowa <ashwinluchowa@gmail.com>
2025-05-04 17:32:17 +0800

    Add image loader

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import "./css/main.scss";
 import { greeting } from "./second.js";
+import odinImage from "./images/image.jpg";
 
 console.log(greeting);
+
+const image = document.createElement('img');
+image.src = odinImage;
+
+document.body.appendChild(image);
